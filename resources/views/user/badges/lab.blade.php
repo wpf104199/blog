48e7f88b31c0f2db964171fd48e1a9df9e124b15
USER-20180107XH\Administrator <48554336@qq.com>
2018-01-18 11:30:55 +0800

    [update] 更新了用户关注功能

--- resources/views/user/badges/lab.blade.php
+++ resources/views/user/badges/lab.blade.php
@@ -1,3 +1,9 @@
+{{dd($target_value->toArray())}}
+@if(\Auth::id()!=$target_value->id)
 <div>
+    @if(\Auth::user()->hasFans($target_value_id))
+    <button class="btn btn-default like-button" like-value="1" like-user="6" _token="MESUY3topeHgvFqsy9EcM916UWQq6khiGHM91wHy" type="button">关注</button>
+@endif
     <button class="btn btn-default like-button" like-value="1" like-user="6" _token="MESUY3topeHgvFqsy9EcM916UWQq6khiGHM91wHy" type="button">取消关注</button>
 </div>
+@endif
